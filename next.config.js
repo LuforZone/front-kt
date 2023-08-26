cdee0796527c33ceb5da3295456ddd603212625e
lufor <1056252297@163.com>
2023-08-27 06:25:27 +0800

    change next config.js and add a catcher

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // 匹配所有以 /api/ 开头的请求
+        destination: 'http://localhost:8080/:path*', // 转发到后端的地址
+      },
+    ];
+  },
+};
